b5139dcf43b33db9fbf405d3b8cad53046ad354b
DotNet4Fun <DotNet4Fun@mail.com>
2020-12-14 20:39:44 +0200

    Mark all Typemock tests using traits such as Typemock|Pragmatic or Typemock|InterfaceOnly - fix when run by xunit.console.exe

---
+++ XMock/Discovery/ReflectionUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace XMock.Discovery
+{
+    internal static class ReflectionUtils
+    {
+        public static SerializationHelper SerializationHelper { get; } = new SerializationHelper();
+    }
+
+    internal class SerializationHelper
+    {
+        private static readonly Type _type;
+        private static readonly MethodInfo _method;
+
+        static SerializationHelper()
+        {
+            _type = typeof(XunitTestFrameworkDiscoverer).Assembly.GetType("Xunit.Sdk.SerializationHelper");
+            _method = _type.GetMethod("GetType", BindingFlags.Static | BindingFlags.Public, null, new[] { typeof(string), typeof(string) }, null);
+        }
+
+        public Type GetType(string assemblyName, string typeName)
+        {
+            return (Type)_method.Invoke(null, new object[] { assemblyName, typeName });
+        }
+    }
+}
--- XMock/Discovery/TestFrameworkDiscoverer.cs
+++ XMock/Discovery/TestFrameworkDiscoverer.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -11,10 +13,43 @@ namespace XMock.Discovery
         {
         }
 
-        public override string Serialize(ITestCase testCase)
+        protected override bool FindTestsForMethod(ITestMethod testMethod, bool includeSourceInformation, IMessageBus messageBus, ITestFrameworkDiscoveryOptions discoveryOptions)
         {
-            UpdateTraits(testCase);
-            return base.Serialize(testCase);
+            // copy-paste from base class...
+
+            var factAttributes = testMethod.Method.GetCustomAttributes(typeof(FactAttribute)).ToList();
+            if (factAttributes.Count > 1)
+            {
+                var message = $"Test method '{testMethod.TestClass.Class.Name}.{testMethod.Method.Name}' has multiple [Fact]-derived attributes";
+                var testCase = new ExecutionErrorTestCase(DiagnosticMessageSink, TestMethodDisplay.ClassAndMethod, testMethod, message);
+                return ReportDiscoveredTestCase(testCase, includeSourceInformation, messageBus);
+            }
+
+            var factAttribute = factAttributes.FirstOrDefault();
+            if (factAttribute == null)
+                return true;
+
+            var testCaseDiscovererAttribute = factAttribute.GetCustomAttributes(typeof(XunitTestCaseDiscovererAttribute)).FirstOrDefault();
+            if (testCaseDiscovererAttribute == null)
+                return true;
+
+            var args = testCaseDiscovererAttribute.GetConstructorArguments().Cast<string>().ToList();
+            var discovererType = ReflectionUtils.SerializationHelper.GetType(args[1], args[0]);
+            if (discovererType == null)
+                return true;
+
+            var discoverer = GetDiscoverer(discovererType);
+            if (discoverer == null)
+                return true;
+
+            foreach (var testCase in discoverer.Discover(discoveryOptions, testMethod, factAttribute))
+            {
+                UpdateTraits(testCase);
+                if (!ReportDiscoveredTestCase(testCase, includeSourceInformation, messageBus))
+                    return false;
+            }
+
+            return true;
         }
 
         private static void UpdateTraits(ITestCase testCase)
